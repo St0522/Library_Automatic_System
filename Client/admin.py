3c9cdb6b7c42e97a938431487f5722312a6a0315
St0522 <steven010522@gmail.com>
2024-05-29 12:09:06 +0900

    modified:   Client/admin.py 	deleted:    Client/adminBookList.py

--- Client/admin.py
+++ Client/admin.py
@@ -4,7 +4,6 @@ from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTableWidget, QT
 from bookList import BookListWindow
 from addBook import AddBookWindow
 from addUser import AddUserWindow
-from adminBookList import addminBookListWindow
 
 class AdminWindow(QWidget):
     def __init__(self):
@@ -95,7 +94,7 @@ class AdminWindow(QWidget):
         self.user_list_window.show()
 
     def showBookListWindow(self):
-        self.book_list_window = addminBookListWindow()
+        self.book_list_window = BookListWindow()
         self.book_list_window.show()
 
     def logout(self):
